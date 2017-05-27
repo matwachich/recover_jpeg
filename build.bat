674ebd482b0ee5453e9fb065a1b8a069a47ce9d3
Samy Matwachich <matwachich@gmail.com>
2017-05-27 18:04:37 +0100

    picture recovered with Option 1 is checked for validity (loaded as JPEG) if valid, user is not asked to confirme

--- build.bat
+++ build.bat
@@ -3,8 +3,10 @@ set GOOS=windows
 
 set GOARCH=386
 go build -o ImageSaver_x86.exe -ldflags "-w -s"
+upx ImageSaver_x86.exe
 
 set GOARCH=amd64
 go build -o ImageSaver_x64.exe -ldflags "-w -s"
+upx ImageSaver_x64.exe
 
 pause
